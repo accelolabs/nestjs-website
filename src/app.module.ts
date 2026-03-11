1b0b3f3addc3082f92b9442b8c2d17ce423a65b3
accelolabs <accelotron@niuitmo.ru>
2026-03-11 16:05:14 +0300

    ORM base + postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ClubsModule } from './clubs/clubs.module';
+import { SeatsModule } from './seats/seats.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'clubs',
+
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    UsersModule,
+    ClubsModule,
+    SeatsModule,
+    BookingsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
